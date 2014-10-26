be3b71b226ca81fabb9550a60a68e978fd53c4a6
teeosx <Tee@Attawees-MacBook-Pro.local>
2014-10-26 14:55:15 +0700

    Project question 1 2 4

---
+++ spreadingfire/Fireforest.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spreadingfire;
+
+/**
+ *
+ * @author Tee
+ */
+public class Fireforest  {
+    Tree[][] tree;
+    static Forest forest;
+    
+    public Fireforest(Forest forest){
+        this.forest = forest;
+        tree = new Tree[forest.getNumTree()][forest.getNumTree()];
+        //System.out.println(forest.getNumTree());
+        for(int i = 0 ; i < forest.getNumTree(); i++){
+            for(int j = 0 ; j < forest.getNumTree() ; j++){
+                tree[i][j] = new Tree();
+                //tree[i][j].setState(0);
+                if(i == forest.getNumTree()/2 && j == forest.getNumTree()/2) {
+                    tree[i][j].setState(2);
+                }
+                
+                if(i == 0 || i == forest.getNumTree()-1 || j == 0 || j == forest.getNumTree()-1){
+                    tree[i][j].setState(0);
+                } 
+            }
+        }
+        forest.update(tree);
+    }
+  
+}
